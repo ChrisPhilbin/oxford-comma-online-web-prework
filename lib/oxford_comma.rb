aeeee7930f86b54e120d3d7dda940d31c9b35979
fortunate-database-4943 <chris@chrisphilbin.net>
2018-11-15 23:32:31 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,8 @@
 def oxford_comma(array)
-  array[-1].insert(0, "and ")
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+   array[-1].insert(0, "and ")
+  end
   array.join(", ")
 end
